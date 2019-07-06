a35fd8d2a6da968a73b3c8791a4a149297d35003
Shalitha Suranga <shalithasuranga@gmail.com>
2019-07-06 19:58:58 +0530

    Get values from api

--- client/src/app/components/team-leaderboard/team-leaderboard.component.ts
+++ client/src/app/components/team-leaderboard/team-leaderboard.component.ts
@@ -10,13 +10,35 @@ import { environment } from '../../../environments/environment';
 export class TeamLeaderboardComponent implements OnInit {
 
 	private teams: any;
+	private malePlayers: any;
+	private femalePlayers: any;
 
 	constructor(private http: HttpClient) { }
 
 	ngOnInit() {
+		this.loadTeamsLeaderboard();
+		this.loadMaleLeaderboard();
+		this.loadFemaleLeaderboard();
+	}
+
+	private loadTeamsLeaderboard() {
 		let topTeamsEndpoint = environment.baseApi + '/api/v1/leaderboard/topteams';
 		this.http.get(topTeamsEndpoint).subscribe((teams: any) => {
 			this.teams = teams;
+		});
+	}
+
+	private loadMaleLeaderboard() {
+		let topMaleEndpoint = environment.baseApi + '/api/v1/leaderboard/topmaleplayers';
+		this.http.get(topMaleEndpoint).subscribe((players: any) => {
+			this.malePlayers = players;
+		});
+	}
+
+	private loadFemaleLeaderboard() {
+		let topFemaleEndpoint = environment.baseApi + '/api/v1/leaderboard/topfemaleplayers';
+		this.http.get(topFemaleEndpoint).subscribe((players: any) => {
+			this.femalePlayers = players;
 		});
 	}
 
