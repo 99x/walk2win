545c5b6a421dcd58416403c0162f11770870d6c3
Athif Shaffy <athif.shaffy@gmail.com>
2019-07-13 07:31:48 +0530

    Updates for front-end flow &  UI changes

--- client/src/app/services/data.service.ts
+++ client/src/app/services/data.service.ts
@@ -1,6 +1,44 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { from } from 'rxjs';
 
+import { environment } from 'src/environments/environment.prod';
 @Injectable()
 export class DataService {
-	// TODO add data service here
+
+	constructor(private http: HttpClient) { }
+
+	private buildHttpOptions(): any {
+		return {
+			headers: new HttpHeaders({
+				Authorization: this.createBearerToken()
+			})
+		};
+	}
+
+	private createBearerToken(): string {
+		return `Bearer ${localStorage.getItem('googleoauth')}`;
+	}
+
+	private createUrl(url: string): string {
+		return environment.baseApi + url;
+	}
+
+	/**
+	 * Get google fit sync data
+	 * @param url the api endpoint url to be called
+	 */
+	getSyncData(url: string) {
+		return from(this.http.get(this.createUrl(url), this.buildHttpOptions()));
+	}
+
+
+	/**
+	 * Get Team leaderboard values for male, female or individual
+	 * @param url the api endpoint url to be called
+	 */
+	public getTeamsLeaderboardValues(url: string) {
+		return from(this.http.get(this.createUrl(url)));
+	}
+
 }
