8ea482d7f9185680eaa39e9cd69fb021711a7c21
Shalitha Suranga <shalithasuranga@gmail.com>
2019-07-14 11:46:05 +0530

    Seperate point calc logic

--- server/constants.js
+++ server/constants.js
@@ -1,6 +1,13 @@
-module.exports.marks = [
+module.exports.teamMarks = [
     [12500, 100],
     [10000, 75],
     [7500, 50],
     [5000, 25]
+];
+
+module.exports.soloMarks = [
+    [15000, 100],
+    [12500, 75],
+    [10000, 50],
+    [7500, 25]
 ];
