b6f1ca2ea4ed4abf163436242a3b51c99b5e0669
Shalitha Suranga <shalithasuranga@gmail.com>
2019-07-15 22:47:15 +0530

    Update auth.js

--- server/auth.js
+++ server/auth.js
@@ -1,8 +1,8 @@
 const auth = {
     googleAuth: {
-        clientID: "xx",
-        clientSecret: "xx",
-        callbackURL: ""
+        clientID: "195162494249-aqt60jap5s951apfubo2auvclde0ge9i.apps.googleusercontent.com",
+        clientSecret: "sgYRHqjEPOaTBd5ZPv2EBNdn",
+        callbackURL: "http://localhost:3003/api/v1/auth/google/callback"
     }
 }
 
