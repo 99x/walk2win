b64874dabe2af2597887790c9f38d030d442f7ba
Athif Shaffy <athif.shaffy@gmail.com>
2019-07-16 18:08:02 +0530

    Added api keys

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
 	production: true,
-	baseApi: 'http://localhost:3003',
+	baseApi: 'http://d32d2ytfhn26li.cloudfront.net/',
 	client_id: 'YOUR_CLIENT_ID',
 	apiKey: 'YOUR_API_KEY'
 };
--- server/auth.js
+++ server/auth.js
@@ -1,8 +1,8 @@
 const auth = {
     googleAuth: {
-        clientID: "xx",
-        clientSecret: "xx",
-        callbackURL: ""
+        clientID: "195162494249-aqt60jap5s951apfubo2auvclde0ge9i.apps.googleusercontent.com",
+        clientSecret: "sgYRHqjEPOaTBd5ZPv2EBNdn",
+        callbackURL: "d32d2ytfhn26li.cloudfront.net/api/v1/auth/google/callback"
     }
 }
 
