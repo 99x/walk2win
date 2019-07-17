531c1ec9818bf7955d783a4e78711cb207841a92
Athif Shaffy <AthifS@99x.lk>
2019-07-17 10:51:16 +0530

    Updated base api

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
 	production: true,
-	baseApi: 'http://18.218.3.5',
+	baseApi: 'http://ec2-18-218-3-5.us-east-2.compute.amazonaws.com',
 	client_id: 'YOUR_CLIENT_ID',
 	apiKey: 'YOUR_API_KEY'
 };
--- server/auth.js
+++ server/auth.js
@@ -2,7 +2,7 @@ const auth = {
     googleAuth: {
         clientID: "195162494249-aqt60jap5s951apfubo2auvclde0ge9i.apps.googleusercontent.com",
         clientSecret: "sgYRHqjEPOaTBd5ZPv2EBNdn",
-        callbackURL: "http://18.218.3.5/api/v1/auth/google/callback"
+        callbackURL: "http://ec2-18-218-3-5.us-east-2.compute.amazonaws.com/api/v1/auth/google/callback"
     }
 }
 
