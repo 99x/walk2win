555bf7928ab612339bed4c11d38c2958f10250a8
Shalitha Suranga <shalithasuranga@gmail.com>
2019-07-17 20:21:57 +0530

    Make leaderboard public

--- server/routes/leaderBoardRouter.js
+++ server/routes/leaderBoardRouter.js
@@ -1,9 +1,7 @@
 let express = require('express');
 let router = express.Router();
 let leaderBoardController = require('../controllers/leaderBoardController');
-let {checkToken} = require("../token-validator");
 
-router.use(checkToken);
 router.get('/api/v1/leaderboard/topteams', leaderBoardController.topTeams);
 router.get('/api/v1/leaderboard/topteams', leaderBoardController.topTeams);
 router.get('/api/v1/leaderboard/topteams/:teamId', leaderBoardController.topTeamPlayers);
