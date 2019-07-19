92362f779286e95e92d0f8dab6e383b7796d7e34
Shalitha Suranga <shalithasuranga@gmail.com>
2019-07-19 22:46:03 +0530

    Implement scoring

--- server/controllers/authController.js
+++
@@ -1,8 +0,0 @@
-let Auth = require('../models/auth');
-const constants = require('../constants');
-
-module.exports.getRefreshToken = (req, res) => {
-    Auth.find({gmail: ""}, async (err, resp) => {
-        
-    });
-}
--- server/helpers/pointCalculator.js
+++ server/helpers/pointCalculator.js
@@ -1,5 +1,6 @@
 let marks = require('../constants');
 let Player = require('../models/player');
+let Team = require('../models/team');
 
 let calculatePoints = (options) => {
     let steps = options.steps;
@@ -28,22 +29,40 @@ let calculatePoints = (options) => {
     return points;
 };
 
-let calculateTotals = (id) => {
-    Player.findById(id, (error, player) => {
-        if(error) return;
-        let totalPoints = player.total_steps.reduce((a, b) => (a + b.points), 0);
-        let totalSteps = player.total_steps.reduce((a, b) => (a + b.steps), 0);
-        Player.updateOne({_id: id}, {points: totalPoints, steps: totalSteps}, () => {});
-    });
+let calculateTotalsSolo = async (id) => {
+    let player = await Player.findOne({_id: id});
+    if(!player) return;
+    let totalPoints = player.total_steps.reduce((a, b) => (a + b.points), 0);
+    let totalSteps = player.total_steps.reduce((a, b) => (a + b.steps), 0);
+    await Player.updateOne({_id: id}, {points: totalPoints, steps: totalSteps});
+    return {
+        totalPoints: totalPoints,
+        totalSteps: totalSteps
+    };
 }
 
-/* 
-console.log(calculatePoints({
-    steps: 22000,
-    type: 'solo'
-})); */
+
+let calculateTotalsTeam = async (id, myId) => {
+    let players = await Player.find({team: id});
+    if(!players) return;
+    let steps = 0;
+    let points = 0;
+    let myRes = {};
+
+    for(let player of players) {
+        let res = await calculateTotalsSolo(player.id);
+        if(player.id == myId) 
+            myRes = res;
+        points += res.totalPoints;
+        steps += res.totalSteps;
+    }
+
+    await Team.updateOne({_id: id}, {steps: steps, points: points});
+    return myRes;
+}
 
 module.exports = {
     calculatePoints: calculatePoints,
-    calculateTotals: calculateTotals
+    calculateTotalsSolo: calculateTotalsSolo,
+    calculateTotalsTeam: calculateTotalsTeam
 };
