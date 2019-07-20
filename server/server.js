c170bbbbbd1a4782ae89e7cfd0ec42afa7503091
Shalitha Suranga <shalithasuranga@gmail.com>
2019-07-20 20:21:37 +0530

    Cleaning some stuff

--- server/server.js
+++ server/server.js
@@ -3,5 +3,5 @@ const app = require('./app');
 const port = process.env.PORT;
 
 const server = app.listen(port, () => {
-    console.log(`Server is listening to  port ${port}`);
+    console.log(`API/Server is listening to  port ${port}`);
 });
