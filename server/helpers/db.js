f7f91f1ffb5e67c2be2fffe54a08a9453c85b30b
Shakya Peiris <shakyaimanjith32@gmail.com>
2021-10-16 10:04:42 +0530

    seperate mock DB connection and main DB connection to seperate functions and implement database initialization for tests in mock DB connect function

--- server/helpers/db.js
+++ server/helpers/db.js
@@ -1,46 +1,71 @@
 const mongoose = require('mongoose');
+// const team = require('../models/team');
+// const player = require('../models/player');
+
+exports.connectMock = (teamArr, playerArr) => {
+  return new Promise((resolve, reject) => {
+    const { MongoMemoryServer } = require('mongodb-memory-server');
+    MongoMemoryServer.create()
+      .then((mongoServer) => {
+        return mongoose.connect(mongoServer.getUri(), {
+          useNewUrlParser: true,
+          useUnifiedTopology: true,
+        });
+      })
+      .then((client) => {
+        const team = client.model('Team');
+        const player = client.model('Player');
+        let selected = {
+          team: '',
+          player: '',
+        };
+        team.insertMany(teamArr).then((teams) => {
+          selected.team =
+            teams[Math.round(Math.random() * 100) % 3]._id.toString();
+          playerArr[0].team =
+            teams[Math.round(Math.random() * 100) % 3]._id.toString();
+          playerArr[1].team =
+            teams[Math.round(Math.random() * 100) % 3]._id.toString();
+          playerArr[2].team =
+            teams[Math.round(Math.random() * 100) % 3]._id.toString();
+          playerArr[3].team =
+            teams[Math.round(Math.random() * 100) % 3]._id.toString();
+          playerArr[4].team =
+            teams[Math.round(Math.random() * 100) % 3]._id.toString();
+          playerArr[5].team =
+            teams[Math.round(Math.random() * 100) % 3]._id.toString();
+          player.insertMany(playerArr).then((players) => {
+            selected.player =
+              players[Math.round(Math.random() * 100) % 6]._id.toString();
+            resolve(selected);
+          });
+        });
+      })
+      .catch((err) => {
+        reject(err);
+      });
+  });
+};
 
 exports.connect = () => {
   return new Promise((resolve, reject) => {
-    if (process.env.NODE_ENV === 'test') {
-      const Mockgoose = require('mockgoose').Mockgoose;
-      const mock = new Mockgoose(mongoose);
-      mock
-        .prepareStorage()
-        .then(() => {
-          mongoose.connect(
-            'mongodb+srv://Shakya:0xHHQ07yRcxKslsC@cluster0.y64qs.mongodb.net/shop?retryWrites=true&w=majority',
-            {
-              useNewUrlParser: true,
-              useUnifiedTopology: true,
-            }
-          );
-        })
-        .then(() => {
-          resolve();
-        })
-        .catch((err) => {
-          reject(err);
-        });
-    } else {
-      mongoose
-        .connect(
-          'mongodb+srv://Shakya:0xHHQ07yRcxKslsC@cluster0.y64qs.mongodb.net/shop?retryWrites=true&w=majority',
-          {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-          }
-        )
-        .then(() => {
-          resolve();
-        })
-        .catch((err) => {
-          reject(err);
-        });
-    }
+    mongoose
+      .connect(
+        'mongodb+srv://Shakya:0xHHQ07yRcxKslsC@cluster0.y64qs.mongodb.net/shop?retryWrites=true&w=majority',
+        {
+          useNewUrlParser: true,
+          useUnifiedTopology: true,
+        }
+      )
+      .then(() => {
+        resolve();
+      })
+      .catch((err) => {
+        reject(err);
+      });
   });
 };
 
 exports.close = () => {
-  return mongoose.disconnect();
+  mongoose.disconnect();
 };
